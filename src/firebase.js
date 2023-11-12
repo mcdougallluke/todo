8b3c7ffcd5b45df122512039268dff278d7c1e0c
Luke <lmcdougall@unm.edu>
2023-11-12 11:54:40 -0700

    fix

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBU06z-HQgKD6kOyu67JSrTFFlfP3eawOE",
-  authDomain: "todo-app-2-7ef26.firebaseapp.com",
-  projectId: "todo-app-2-7ef26",
-  storageBucket: "todo-app-2-7ef26.appspot.com",
-  messagingSenderId: "580107915135",
-  appId: "1:580107915135:web:c449c3f38073cd6a8b944a"
-}
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 
-const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
