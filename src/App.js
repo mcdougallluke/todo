5085e19dff26b586aa428660d508c496ad202188
Luke <lmcdougall@unm.edu>
2023-11-14 14:35:37 -0700

    fixed bg bug and checked bug

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ import {
 } from 'firebase/firestore'
 
 const style = {
-    bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#56CCF2]`,
+    bg: `min-h-screen min-w-screen tp-4 bg-gradient-to-r from-[#2F80ED] to-[#56CCF2]`,
     container: `bg-slate-100 max-w-[600px] w-full m-auto rounded-md shadow-xl p-4`,
     heading: `text-3xl font-bold text-center text-gray-800 p-2`,
     form: `flex justify-between`,
     input: `border p-2 w-full text-xl`,
     button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
-    count: `text-center p-2`
+    count: `text-center p-2 text-xl`
 }
 
 function App() {
@@ -66,7 +66,7 @@ function App() {
   return (
     <div className={style.bg}>
         <div className={style.container}>
-            <h3 className={style.heading}>Todo App</h3>
+            <h3 className={style.heading}>Todo List</h3>
             <form onSubmit={createTodo} className={style.form}>
                 <input
                     value={input} 
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
-import { FaRegTrashAlt } from 'react-icons/fa'
+import { FaRegTrashAlt, FaCheckCircle, FaRegCircle } from 'react-icons/fa'
 
 
 const style = {
-    li: `flex justify-between bg-slate-200 p-4 my-2 capitalize rounded-md`,
-    liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize rounded-md`,
-    row: `flex`,
+    li: `flex justify-between items-center bg-slate-200 p-4 my-2 capitalize rounded-md`,
+    liComplete: `flex justify-between items-center bg-slate-400 p-4 my-2 capitalize rounded-md`,
+    row: `flex items-center`,
     text: `text-xl ml-2 cursor-pointer`,
-    textComplete: `cursor-pointer ml-2 line-through`,
+    textComplete: `cursor-pointer ml-2 line-through text-xl`,
     button: `cursor-pointer flex items-center`,
-}
+    checkbox: `cursor-pointer mr-2`, 
+};
 
 const Todo = ({ todo, toggleComplete, deleteTodo }) => {
   return (
     <li className={todo.completed ? style.liComplete : style.li}>
-        <div className={style.row}>
-            <input onChange={() => toggleComplete(todo)} type="checkbox" checked={todo.completed ? 'checked': ''}/>
-            <p onClick={() => toggleComplete(todo)} className={todo.completed ? style.textComplete : style.text}>{todo.text}</p>
-        </div>
-        <button onClick={() => deleteTodo(todo.id)}><FaRegTrashAlt /></button>
-    </li>
+            <div className={style.row}>
+                <div onClick={() => toggleComplete(todo)} className={style.checkbox}>
+                    {todo.completed ? <FaCheckCircle size={20} /> : <FaRegCircle size={20} />}
+                </div>
+                <p onClick={() => toggleComplete(todo)} className={todo.completed ? style.textComplete : style.text}>
+                    {todo.text}
+                </p>
+            </div>
+            <button onClick={() => deleteTodo(todo.id)} className={style.button}><FaRegTrashAlt /></button>
+        </li>
   )
 }
 
