b605460597283a15d0a176995d1f87c9567da458
Luke <lmcdougall@unm.edu>
2023-11-14 14:43:39 -0700

    fixed container spacing

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ import {
 } from 'firebase/firestore'
 
 const style = {
-    bg: `min-h-screen min-w-screen tp-4 bg-gradient-to-r from-[#2F80ED] to-[#56CCF2]`,
-    container: `bg-slate-100 max-w-[600px] w-full m-auto rounded-md shadow-xl p-4`,
+    bg: `min-h-screen min-w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#56CCF2]`,
+    container: `bg-slate-100 max-w-[600px] w-11/12 md:w-full m-auto rounded-md shadow-xl p-4`,
     heading: `text-3xl font-bold text-center text-gray-800 p-2`,
     form: `flex justify-between`,
     input: `border p-2 w-full text-xl`,
